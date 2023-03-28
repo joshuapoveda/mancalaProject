4c2be500e9986d031d4b48e739c8d324ba2230d6
Joshua Poveda <joshuapoveda9@gmail.com>
2023-03-28 16:06:28 -0400

    Update: nucleus (player pieces) created

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('test')
+let boardCellsPlayerOne = document.querySelectorAll("#player-one");
+console.log(boardCellsPlayerOne);
+for (elem of boardCellsPlayerOne) {
+  for (let i = 0; i < 4; i++) {
+    let nucleusOne = document.createElement("div");
+    nucleusOne.textContent = "nucleus";
+    nucleusOne.classList.add('playerOneNucleus')
+    elem.append(nucleusOne);
+    console.log(nucleusOne)
+  }
+}
+
+let boardCellsPlayerTwo = document.querySelectorAll("#player-two");
+console.log(boardCellsPlayerTwo);
+for (elem of boardCellsPlayerTwo) {
+  for (let i = 0; i < 4; i++) {
+    let nucleusTwo = document.createElement("div");
+    nucleusTwo.textContent = "nucleus";
+    nucleusTwo.classList.add('playerTwoNucleus')
+    elem.append(nucleusTwo);
+    console.log(nucleusTwo)
+  }
+}
+
