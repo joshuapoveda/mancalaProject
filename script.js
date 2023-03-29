2681ab5105898fdef656e6dbc15bf2b07965df89
Joshua Poveda <joshuapoveda9@gmail.com>
2023-03-29 15:02:29 -0400

    added early move-pieces func, To Do: add eventListene,/su- divs for each player

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ class GameMechanics {
     for (let elem of boardCellsPlayerOne) {
       for (let i = 0; i < 4; i++) {
         let nucleusOne = document.createElement("div");
-        nucleusOne.textContent = "P1-nucleus";
+        nucleusOne.textContent = "P1-nucleus" + (i + 1);
         nucleusOne.classList.add("playerOneNucleus");
         elem.append(nucleusOne);
       }
@@ -13,22 +13,23 @@ class GameMechanics {
     for (let elem of boardCellsPlayerTwo) {
       for (let i = 0; i < 4; i++) {
         let nucleusTwo = document.createElement("div");
-        nucleusTwo.textContent = "P2-nucleus";
+        nucleusTwo.textContent = "P2-nucleus" + (i + 1);
         nucleusTwo.classList.add("playerTwoNucleus");
         elem.append(nucleusTwo);
       }
     }
   }
 }
+const setNewGame = new GameMechanics();
+setNewGame.setBoard();
 
-const setNewGame = new GameMechanics
-setNewGame.setBoard()
+const currentCell = document.querySelector("#p1-cell-1");
 
-const currentCell = document.querySelector("#p1-cell-a");
-currentCellChild = currentCell.children;
-//.children returns element children only, ignoring text content, unlike childNodes
-//console.log(currentCellChild)
-
-if (currentCellChild.length === 4) {
-  console.log("test");
-}
+currentCell.addEventListener("click", function () {
+  let currentCellChild = currentCell.children;
+  if (currentCellChild.length != 0) {
+    for (let i = 2; i < 6; i++) {
+      document.querySelector(`#p1-cell-${i}`).append(currentCellChild[0]);
+    }
+  }
+});
