a76264dbcf60919ae9f7fa18c122bdf564edf2e1
Joshua Poveda <joshuapoveda9@gmail.com>
2023-03-29 11:27:00 -0400

    added set board method in GameMechanics class

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
-let boardCellsPlayerOne = document.querySelectorAll("#player-one");
-console.log(boardCellsPlayerOne);
-for (elem of boardCellsPlayerOne) {
-  for (let i = 0; i < 4; i++) {
-    let nucleusOne = document.createElement("div");
-    nucleusOne.textContent = "nucleus";
-    nucleusOne.classList.add('playerOneNucleus')
-    elem.append(nucleusOne);
-    console.log(nucleusOne)
+class GameMechanics {
+  setBoard() {
+    let boardCellsPlayerOne = document.querySelectorAll(".player-one");
+    for (let elem of boardCellsPlayerOne) {
+      for (let i = 0; i < 4; i++) {
+        let nucleusOne = document.createElement("div");
+        nucleusOne.textContent = "P1-nucleus";
+        nucleusOne.classList.add("playerOneNucleus");
+        elem.append(nucleusOne);
+      }
+    }
+    let boardCellsPlayerTwo = document.querySelectorAll(".player-two");
+    for (let elem of boardCellsPlayerTwo) {
+      for (let i = 0; i < 4; i++) {
+        let nucleusTwo = document.createElement("div");
+        nucleusTwo.textContent = "P2-nucleus";
+        nucleusTwo.classList.add("playerTwoNucleus");
+        elem.append(nucleusTwo);
+      }
+    }
   }
 }
 
-let boardCellsPlayerTwo = document.querySelectorAll("#player-two");
-console.log(boardCellsPlayerTwo);
-for (elem of boardCellsPlayerTwo) {
-  for (let i = 0; i < 4; i++) {
-    let nucleusTwo = document.createElement("div");
-    nucleusTwo.textContent = "nucleus";
-    nucleusTwo.classList.add('playerTwoNucleus')
-    elem.append(nucleusTwo);
-    console.log(nucleusTwo)
-  }
-}
+const setNewGame = new GameMechanics
+setNewGame.setBoard()
 
+const currentCell = document.querySelector("#p1-cell-a");
+currentCellChild = currentCell.children;
+//.children returns element children only, ignoring text content, unlike childNodes
+//console.log(currentCellChild)
+
+if (currentCellChild.length === 4) {
+  console.log("test");
+}
