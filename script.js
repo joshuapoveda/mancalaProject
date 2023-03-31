1a3e0beb0c8dbc49be2463251b3cef94e07cc492
Joshua Poveda <joshuapoveda9@gmail.com>
2023-03-31 15:32:52 -0400

    small bug in loop (duplicates),TODO: scoring functionality, refine player turn cycle

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ class GameMechanics {
     for (let elem of boardCellsPlayerOne) {
       for (let i = 0; i < 4; i++) {
         let nucleusOne = document.createElement("div");
-        nucleusOne.textContent = "P1-nucleus" + (i + 1);
+        nucleusOne.textContent = `*`;
         nucleusOne.classList.add("playerOneNucleus");
         elem.append(nucleusOne);
       }
@@ -13,39 +13,46 @@ class GameMechanics {
     for (let elem of boardCellsPlayerTwo) {
       for (let i = 0; i < 4; i++) {
         let nucleusTwo = document.createElement("div");
-        nucleusTwo.textContent = "P2-nucleus" + (i + 1);
+        nucleusTwo.textContent = `*`;
         nucleusTwo.classList.add("playerTwoNucleus");
         elem.append(nucleusTwo);
       }
     }
   }
 
+  resetBoard() {
+    location.reload();
+  }
+
   playerOneTurn() {
     const board = document.querySelector("#board");
 
     board.addEventListener("click", function (event) {
+      // let img = document.createElement("img");
+      // img.src = 'styles/imgs/images.png'
+      // img.style.height = '50px'
+      // img.style.width = '50px'
       let currentCell = event.target;
-      console.log(currentCell);
       let currentCellId = +currentCell.getAttribute("id");
-      console.log(currentCellId);
-      let cells = Array.from(board.children)
-      console.log(cells);
-      let nuclei = currentCell.children.length;
-      console.log(nuclei);
-
-      cells.sort(function(a,b){
-        return a.id - b.id
-      })
-
-    
-
-      for (let i = 0; i < nuclei; i++) {
-        if ((currentCellId + nuclei) % 14 > 10) {
-          cells[currentCellId + i].append("TEST");
+      let cells = Array.from(board.children);
+      //let nuclei = currentCell.children
+      let numOfnuclei = currentCell.children.length;
+      if (currentCell.classList.contains("player-two")) {
+        return;
+      }
+      cells.sort(function (a, b) {
+        return a.id - b.id;
+      });
+      let cnt = 0;
+      for (let i = 0; i < numOfnuclei; i++) {
+        currentCell.innerHTML = ''
+        cnt++;
+        if (currentCellId + i < 14) {
+          cells[currentCellId + i].append('*');
         } else {
-          let remainder = nuclei - i;
-          for (let i = 0; i < remainder; i++) {
-            cells[6].append('*')
+          let remainder = numOfnuclei - cnt;
+          for (let i = 0; i <= remainder; i++) {
+            cells[i].append('*');
           }
         }
       }
@@ -53,23 +60,40 @@ class GameMechanics {
   }
 
   playerTwoTurn() {
-    const boards = document.querySelector("#board");
+    const board = document.querySelector("#board");
 
-    boards.addEventListener("click", function (event) {
-      let currentCellChild = event.target.children;
-      if (event.target.classList == "player-two") {
-        console.log("testP2");
-      } else {
+    board.addEventListener("click", function (event) {
+      let currentCell = event.target;
+      let currentCellId = +currentCell.getAttribute("id");
+      let cells = Array.from(board.children);
+      let numOfnuclei = currentCell.children.length;
+      if (currentCell.classList.contains("player-one")) {
         return;
       }
-      if (currentCellChild.length != 0) {
-        for (let i = 2; i < 6; i++) {
-          document.querySelector(`#p2-cell-${i}`).append(currentCellChild[0]);
+      cells.sort(function (a, b) {
+        return a.id - b.id;
+      });
+
+      let cnt = 0;
+      for (let i = 0; i < numOfnuclei; i++) {
+        cnt++;
+        if (currentCellId + i < 14) {
+          cells[currentCellId + i].append("*");
+        } else if (currentCellId + i >= 14) {
+          let remainder = numOfnuclei - cnt;
+          for (let i = 0; i <= remainder; i++) {
+            cells[i].append("*");
+          }
         }
       }
     });
   }
 }
+
+const resetBtn = document.querySelector(".reset-btn");
+resetBtn.addEventListener("click", function (event) {
+  NewGame.resetBoard();
+});
 
 const NewGame = new GameMechanics();
 NewGame.setBoard();
