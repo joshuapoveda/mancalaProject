a5c14eb14952e2b36460b5bcba8632ba65c27627
Joshua Poveda <joshuapoveda9@gmail.com>
2023-04-01 23:39:48 -0400

    both sides of board working

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ class GameMechanics {
   setBoard() {
     let boardCellsPlayerOne = document.querySelectorAll(".player-one");
     for (let elem of boardCellsPlayerOne) {
-      for (let i = 0; i < 2; i++) {
+      for (let i = 0; i < 4; i++) {
         let nucleusOne = document.createElement("div");
         nucleusOne.textContent = `*${i + 1}`;
         nucleusOne.classList.add("playerOneNucleus");
@@ -73,40 +73,67 @@ class GameMechanics {
           for (let i = 1; i <= numOfnuclei; i++) {
             playerOneBoard[currentCellId + i].append("0");
             //Below is the case for player to go again
-            console.log(playerOneBoard[currentCellId + numOfnuclei])
+            console.log(playerOneBoard[currentCellId + numOfnuclei]);
           }
         }
       }
     });
   }
-
   playerTwoTurn() {
     const board = document.querySelector("#board");
 
     board.addEventListener("click", function (event) {
       let currentCell = event.target;
-      let currentCellId = +currentCell.getAttribute("id");
-      let cells = Array.from(board.children);
-      let numOfnuclei = currentCell.children.length;
       if (currentCell.classList.contains("player-one")) {
         return;
       }
+      console.log(currentCell);
+
+      let currentCellId = +currentCell.getAttribute("id");
+      console.log(currentCellId);
+
+      let cells = Array.from(board.children);
       cells.sort(function (a, b) {
         return a.id - b.id;
       });
+      console.log(cells);
 
-      let cnt = 0;
-      for (let i = 0; i < numOfnuclei; i++) {
-        cnt++;
-        if (currentCellId + i < 14) {
-          cells[currentCellId + i].append("*");
-        } else if (currentCellId + i >= 14) {
-          let remainder = numOfnuclei - cnt;
-          for (let i = 0; i <= remainder; i++) {
-            cells[i].append("*");
+      let nuclei = Array.from(currentCell.children);
+      console.log(nuclei);
+
+      let numOfnuclei = nuclei.length;
+      console.log(numOfnuclei);
+
+      const playerTwoBoard = cells.filter(removeP1Store);
+      function removeP1Store(cell) {
+        return cell.getAttribute("id") != 6;
+      }
+      console.log(playerTwoBoard);
+
+      if (numOfnuclei > 0) {
+        if (numOfnuclei + currentCellId > 13) {
+          let diff = 13 - currentCellId;
+          console.log(diff);
+          for (let i = 0; i < diff; i++) {
+            playerTwoBoard[currentCellId + i].append("0");
+          }
+
+          let remainder = numOfnuclei - diff - 1;
+          console.log(remainder);
+
+          for (let j = 0; j <= remainder; j++) {
+            playerTwoBoard[j].append("0");
+            //Below is the case for player to go again
+            //console.log(playerOneBoard[remainder - 1]);
+          }
+        } else {
+          for (let i = 1; i <= numOfnuclei; i++) {
+            playerTwoBoard[i + currentCellId - 1].append("0");
+            //Below is the case for player to go again
+            //console.log(playerTwoBoard[currentCellId + numOfnuclei]);
           }
         }
-      }
+      } 
     });
   }
 }
@@ -119,5 +146,5 @@ resetBtn.addEventListener("click", function (event) {
 const NewGame = new GameMechanics();
 NewGame.setBoard();
 
-NewGame.playerOneTurn();
-//NewGame.playerTwoTurn();
+//NewGame.playerOneTurn();
+NewGame.playerTwoTurn();
